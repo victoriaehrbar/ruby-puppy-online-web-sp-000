44d9cf7ce2a34c69bc649020a584b52d6f660ac1
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-04 02:30:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,12 +4,23 @@ class Dog
   @@all = []
 
   def initialize(name)
-   @name = name
-   save
- end
+    @name = name
+    save
+  end
 
- def self.all
+  def self.all
     @@all
   end
-  
+
+  def self.print_all
+    puts @@all.map{ |dog| dog.name }
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
 end
